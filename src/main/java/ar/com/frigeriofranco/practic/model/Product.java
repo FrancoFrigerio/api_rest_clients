05c1c720c8d37a4826cafd82713591cdd26261e7
FRANCO <franco.p.f@live.com.ar>
2021-12-11 20:55:44 -0300

    producto repository added

---
+++ src/main/java/ar/com/frigeriofranco/practic/model/Product.java
@@ -0,0 +1,43 @@
+package ar.com.frigeriofranco.practic.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private Double price;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date created_At;
+
+
+    private String urlPhoto1;
+
+
+    private String urlPhoto2;
+
+
+}
