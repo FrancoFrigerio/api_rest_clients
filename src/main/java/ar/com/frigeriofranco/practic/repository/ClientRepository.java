c26d5d4bdecddc90e125c3910f695eae9eb61304
FRANCO <franco.p.f@live.com.ar>
2022-01-05 13:44:40 -0300

    getMetrics and getTotal

--- src/main/java/ar/com/frigeriofranco/practic/repository/ClientRepository.java
+++ src/main/java/ar/com/frigeriofranco/practic/repository/ClientRepository.java
@@ -4,6 +4,7 @@ import ar.com.frigeriofranco.practic.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Long> {
@@ -12,5 +13,11 @@ public interface ClientRepository extends JpaRepository<Client,Long> {
 
     @Query(value = "select sum(b.total) from bills b inner join clients c on c.id = b.cliente_id where c.id=?1", nativeQuery = true)
     Double sumBills(Long id);
+
+    @Query(value = "select c.* ,sum(b.total) from clients c inner join bills b on c.id = b.cliente_id group by c.name order by sum(b.total) desc limit 3",nativeQuery = true)
+    List<Client> getMetrics ();
+
+    @Query(value = "select sum(b.total) from clients c inner join bills b on b.cliente_id = c.id where c.id =?1",nativeQuery = true)
+    Double getSaldo(Long id);
 
 }
