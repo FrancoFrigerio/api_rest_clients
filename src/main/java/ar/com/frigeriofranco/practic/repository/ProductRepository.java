269efc83eaff13ba3167696d4b61853772c462c6
FRANCO <franco.p.f@live.com.ar>
2022-01-14 19:04:14 -0300

    sql queri added

--- src/main/java/ar/com/frigeriofranco/practic/repository/ProductRepository.java
+++ src/main/java/ar/com/frigeriofranco/practic/repository/ProductRepository.java
@@ -12,4 +12,7 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
 
     @Query(value = "SELECT * from products where name like %?1%",nativeQuery = true)
     List<Product> getProductLike(String name);
+
+    @Query(value = "select  p.id, p.name,sum(i.count)from items i inner join products p on i.product_id = p.id inner join bills b on i.bill_id = b.id where b.created_at between '2021-08-01'and'2022-01-31' group by p.name" ,nativeQuery = true)
+    List<List<String>> getMetrics();
 }
